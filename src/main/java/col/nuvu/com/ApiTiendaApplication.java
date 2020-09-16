3cf437788fb4160c8ecabf5ad3deac8466274dda
Erick <erickjhorman@gmail.com>
2020-09-16 01:20:27 -0500

    first commit

---
+++ src/main/java/col/nuvu/com/ApiTiendaApplication.java
@@ -0,0 +1,13 @@
+package col.nuvu.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTiendaApplication.class, args);
+	}
+
+}
